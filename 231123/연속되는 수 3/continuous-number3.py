2754bca426e1ad8c2f259b4fcd0447ca696ddbf0
moho191113 <91732306+moho191113@users.noreply.github.com>
2023-11-23 20:54:55 +0900

    Title: 연속되는 수 3, Time: 164ms, Memory: 24MB, Status: Passed - CodeTree

--- 231123/연속되는 수 3/continuous-number3.py
+++ 231123/연속되는 수 3/continuous-number3.py
@@ -1,5 +1,7 @@
+# n개의 데이터
 n = int(input())
 
+# n개의 데이터 저장
 array = [
   int(input())
   for _ in range(n)
@@ -12,13 +14,17 @@ def ne_po_dis(num) :
   else :
     return 0
 
+# 연속수열 기록
 ans, cnt = 0, 0
 
 for i in range(n) :
+  # 이전값이 똑같은 음수면 + 1
   if i >= 1 and ne_po_dis(array[i]) == ne_po_dis(array[i - 1]) :
     cnt += 1
+  # 아니면 1로 초기화
   else :
     cnt = 1
+  # 최대 연속수열 기록
   ans = max(ans, cnt)
 
 print(ans)
