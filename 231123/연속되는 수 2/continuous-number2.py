d73e6ccf50a7775c00926d24e41a75d3785488f4
moho191113 <91732306+moho191113@users.noreply.github.com>
2023-11-23 20:28:09 +0900

    Title: 연속되는 수 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 231123/연속되는 수 2/continuous-number2.py
+++ 231123/연속되는 수 2/continuous-number2.py
@@ -3,18 +3,23 @@
 # n 개의 수 입력 받기
 n = int(input())
 
+# 배열에 input을 저장
 array = [
   int(input())
   for _ in range(n)
 ]
 
+# 연속해서 동일한 숫자가 나오는 횟수중 회대
 max_cnt = 0
 
 for i in range(n) :
+  # 만약 index가 0이거나 이전 숫자와 동일하지 않으면 cnt를 1로 초기화 
   if i == 0 or array[i] != array[i - 1] :
     cnt = 1
+  # 그렇지 않으면 동일한 숫자라는 의미이기 때문에 cnt + 1
   else :
     cnt += 1
+  # max_cnt 와 비교해서 cnt가 더 크면 업데이트
   max_cnt = max(max_cnt, cnt)
-  
+
 print(max_cnt)
