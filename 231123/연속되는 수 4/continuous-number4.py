019d4a1b90da2ee0750b17b84d4b6c8a45a7a258
moho191113 <91732306+moho191113@users.noreply.github.com>
2023-11-23 21:15:45 +0900

    Title: 연속되는 수 4, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231123/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,23 @@
+# n개의 데이터 받기
+n = int(input())
+
+# array에 데이터 받기
+array = [
+  int(input())
+  for _ in range(n)
+]
+
+# 최대 연속수열 저장
+ans, cnt = 0, 0
+
+for i in range(n) :
+  # 연속수열이면 cnt + 1
+  if i > 1 and array[i] > array[i - 1] : 
+    cnt += 1
+  # 연속수열 아니면 1
+  else :
+    cnt = 1
+  # 최대 연속수열 업데이트
+  ans = max(ans, cnt)
+
+print(ans)
