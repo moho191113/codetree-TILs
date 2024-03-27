d7a2597ec21be85f022fd00aa2900358312ecea7
moho191113 <91732306+moho191113@users.noreply.github.com>
2024-03-27 15:45:12 +0900

    Title: 합성수 판별기, Time: 222ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240327/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        boolean satisfied = false;
+        if (n > 2) {
+            for (int i = 2; i <= (n - 1); i++) {
+                if ( n % i == 0) {
+                    satisfied = true;
+                    break;
+                }
+            }
+        }
+
+        if (satisfied == true) {
+            System.out.println("C");
+        } else {
+            System.out.println("N");
+        }
+    }
+}
