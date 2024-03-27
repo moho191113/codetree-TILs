32e1a9981348cce0f55112ad852d957e3094098c
moho191113 <91732306+moho191113@users.noreply.github.com>
2024-03-27 10:10:49 +0900

    Title: 문자 받아 출력, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240327/문자 받아 출력/enter-char-and-print.java
+++ 240327/문자 받아 출력/enter-char-and-print.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        char c = sc.next().charAt(0);
+        Scanner sc = new Scanner(System.in); // 키보드 입력
+        char c = sc.next().charAt(0);        // 문자열의 에서 첫번째 값
 
         System.out.println(c);
     }
