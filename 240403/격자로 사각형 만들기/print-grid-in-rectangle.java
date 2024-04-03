acfb648c5e369d6eaacfcff2b802b35570e09e1a
moho191113 <91732306+moho191113@users.noreply.github.com>
2024-04-03 13:22:55 +0900

    Title: 격자로 사각형 만들기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240403/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        // 1. 첫번째 행 전부 1로 채워주기
+        for (int i =0; i < arr.length; i++ ) {
+            arr[0][i] = 1;
+        }
+
+        // 2. 첫번째 열 전부 1로 채워주기
+        for (int i = 0; i < arr.length; i++ ) {
+            arr[i][0] = 1;
+        }
+
+        // 3. 바로 위칸과 바로 왼쪽값을 더함
+        for (int i = 1; i < arr.length; i++){
+            for (int j = 1; j < arr.length; j++) {
+                arr[i][j] = (arr[i-1][j] + arr[i][j-1] + arr[i-1][j-1]); // 바로 위칸과 바로 왼쪽값을 더함
+            }
+        }
+
+        // 4. 출력
+        for (int i = 0; i < arr.length; i++ ){
+            for (int j = 0; j < arr.length; j++ ){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
